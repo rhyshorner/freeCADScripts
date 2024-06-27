c31e86970ff5b3fa11ac6f27c245369b88576a16
rhyshorner <rhyshorner@gmail.com>
2024-06-27 13:27:57 +1000

    updating bspline

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 These are just some script's I created for myself to figure out how to use python in FreeCAD.
+
+
+pip install freecad-stubs
--- bSplineMacro.py
+++ bSplineMacro.py
@@ -9,32 +9,36 @@ theDocument = App.newDocument()
 sketch = theDocument.addObject("Sketcher::SketchObject", "MacroSketch")
 
 # the meat
-sketch.addGeometry(Part.Circle(App.Vector(0.000000,0.000000,0),App.Vector(0,0,1),10),True)
+theCircle = sketch.addGeometry(Part.Circle(App.Vector(0.000000,0.000000,0),App.Vector(0,0,1),10),True)
+print(f"theCircle: {theCircle}")
 sketch.addConstraint(Sketcher.Constraint('Weight',0,1.000000))
 sketch.addConstraint(Sketcher.Constraint('Coincident',0,3,-1,1))
+# try to delete the circle
 
-sketch.addGeometry(Part.Circle(App.Vector(28.3069,4.7619,0),App.Vector(0,0,1),10),True)
+theCircle = sketch.addGeometry(Part.Circle(App.Vector(28.3069,4.7619,0),App.Vector(0,0,1),10),True)
+print(f"theCircle: {theCircle}")
 sketch.addConstraint(Sketcher.Constraint('Equal',0,1)) 
+# try to delete the circle
 
-sketch.addGeometry(Part.Circle(App.Vector(49.7354,15.3439,0),App.Vector(0,0,1),10),True)
+theCircle = sketch.addGeometry(Part.Circle(App.Vector(49.7354,15.3439,0),App.Vector(0,0,1),10),True)
+print(f"theCircle: {theCircle}")
 sketch.addConstraint(Sketcher.Constraint('Equal',0,2)) 
+# try to delete the circle
 
 sketch.addGeometry(Part.BSplineCurve(
     [
         App.Vector(0,0),
         App.Vector(28.3069,4.7619),
-        App.Vector(49.7354,15.3439),
-        App.Vector(75.3968,35.1852)
+        App.Vector(49.7354,15.3439)
     ],None,None,False,3,None,False),False)
 
 conList = []
-conList.append(Sketcher.Constraint('InternalAlignment:Sketcher::BSplineControlPoint',0,3,4,0))
-conList.append(Sketcher.Constraint('InternalAlignment:Sketcher::BSplineControlPoint',1,3,4,1))
-conList.append(Sketcher.Constraint('InternalAlignment:Sketcher::BSplineControlPoint',2,3,4,2))
-conList.append(Sketcher.Constraint('InternalAlignment:Sketcher::BSplineControlPoint',3,3,4,3))
+conList.append(Sketcher.Constraint('InternalAlignment:Sketcher::BSplineControlPoint',0,3,3,0))
+conList.append(Sketcher.Constraint('InternalAlignment:Sketcher::BSplineControlPoint',1,3,3,1))
+conList.append(Sketcher.Constraint('InternalAlignment:Sketcher::BSplineControlPoint',2,3,3,2))
 del conList
 
-sketch.exposeInternalGeometry(4)
+sketch.exposeInternalGeometry(3)
 
 """
 App.getDocument('Unnamed').getObject('Sketch').addGeometry(Part.Circle(App.Vector(0.000000,0.000000,0),App.Vector(0,0,1),10),True)
